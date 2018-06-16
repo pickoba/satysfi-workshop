8b8118c2c8c1b0cadfd4ca54b718ff1b19c12731
wraikny <writtenknown@gmail.com>
2018-06-17 00:09:16 +0900

    copy script

---
+++ copy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp -rf snippets ~/.vscode/extensions/wraikny-vscode.satysfi/.
+cp -rf syntaxes ~/.vscode/extensions/wraikny-vscode.satysfi/.
+cp -rf snippets ~/.vscode/extensions/wraikny-vscode.satysfi/.
+cp CHANGELOG.md ~/.vscode/extensions/wraikny-vscode.satysfi/.
+cp language-configuration.json ~/.vscode/extensions/wraikny-vscode.satysfi/.
+cp package.json ~/.vscode/extensions/wraikny-vscode.satysfi/.
+cp README.md ~/.vscode/extensions/wraikny-vscode.satysfi/.
