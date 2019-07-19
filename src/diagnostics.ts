99df381fce79c57a4b57c62291d341d47721a30a
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2019-07-19 22:33:59 +0900

    clear diagnostics when no error is found

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -172,6 +172,10 @@ export default class SATySFiProvider implements Disposable {
       }
     }
 
+    if (diagnostics.size == 0) {
+      this.collection.clear();
+      return;
+    }
     diagnostics.forEach((ds, key) => {
       if (!fp.isAbsolute(key)) {
         key = fp.join(fp.dirname(origin), key);
