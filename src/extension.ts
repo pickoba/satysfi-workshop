8edc88ab6a49f4a6253ae955e38fb445e8ba1ff3
KOBAYASHI Ryota <koba.pic@gmail.com>
2021-11-23 22:40:31 +0900

    add language client

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import { commands, ExtensionContext } from "vscode";
 import { Builder } from "./builder";
 import { COMMAND_BUILD, COMMAND_OPEN_BUILD_LOG } from "./const";
 import { DiagnosticsProvider } from "./diagnostics";
+import { LanguageServer } from "./languageServer";
 import { Logger } from "./logger";
 import { StatusBar } from "./statusbar";
 
@@ -21,6 +22,9 @@ export async function activate(extContext: ExtensionContext) {
 
   const diagnosticsProvider = new DiagnosticsProvider(context);
   extContext.subscriptions.push(diagnosticsProvider);
+
+  const languageServer = new LanguageServer(context);
+  extContext.subscriptions.push(languageServer);
 
   commands.registerCommand(COMMAND_BUILD, () => builder.buildProject());
   commands.registerCommand(COMMAND_OPEN_BUILD_LOG, () => context.logger.showBuildLog());
---
+++ src/languageServer.ts
@@ -0,0 +1,89 @@
+import { Disposable, workspace } from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+import { Context } from "./extension";
+import { Logger } from "./logger";
+import { getConfig } from "./util";
+
+export class LanguageServer implements Disposable {
+  private enabled: boolean;
+  private path: string;
+  private client: LanguageClient | null = null;
+  private readonly disposables: Disposable[] = [];
+  private readonly logger: Logger;
+
+  constructor(context: Context) {
+    this.logger = context.logger;
+
+    const config = getConfig();
+    this.enabled = config.languageServer.enabled;
+    this.path = config.languageServer.path;
+
+    this.disposables.push(
+      workspace.onDidChangeConfiguration((_) => {
+        const config = getConfig();
+
+        if (config.languageServer.path !== this.path) {
+          if (this.enabled) {
+            this.stopServer();
+            this.startServer();
+          }
+        }
+
+        if (config.languageServer.enabled !== this.enabled) {
+          if (config.languageServer.enabled) {
+            this.startServer();
+          } else {
+            this.stopServer();
+          }
+          this.enabled = config.languageServer.enabled;
+        }
+      }),
+    );
+
+    if (this.enabled) this.startServer();
+  }
+
+  private startServer() {
+    const serverPath = getConfig().languageServer.path;
+
+    const serverOptions: ServerOptions = {
+      run: { command: serverPath, transport: TransportKind.pipe },
+      debug: {
+        command: serverPath,
+        transport: TransportKind.pipe,
+      },
+    };
+
+    const clientOptions: LanguageClientOptions = {
+      documentSelector: [{ scheme: "file", language: "satysfi" }],
+    };
+
+    this.client = new LanguageClient(
+      "satysfi-language-server",
+      "SATySFi Language Server",
+      serverOptions,
+      clientOptions,
+    );
+
+    this.client.start();
+
+    this.logger.log(`Language Server: start ${serverPath}`);
+  }
+
+  private async stopServer() {
+    await this.client?.stop();
+    this.client = null;
+
+    this.logger.log("Language Server: stop");
+  }
+
+  public dispose() {
+    this.stopServer();
+    this.disposables.forEach((d) => d.dispose());
+  }
+}
