7a28371edcdf2ad9786e2d04caae06cd441125da
KOBAYASHI Ryota <koba.pic@gmail.com>
2021-11-24 00:04:12 +0900

    clean up .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,8 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "@typescript-eslint/member-delimiter-style": [
-      "warn",
-      {
-        multiline: {
-          delimiter: "semi",
-          requireLast: true,
-        },
-        singleline: {
-          delimiter: "semi",
-          requireLast: false,
-        },
-      },
-    ],
-    "@typescript-eslint/naming-convention": "warn",
-    "@typescript-eslint/no-unused-expressions": "warn",
-    "@typescript-eslint/semi": ["warn", "always"],
-    curly: ["warn", "multi-line"],
-    eqeqeq: ["warn", "always"],
-    "no-redeclare": "warn",
-    "no-throw-literal": "warn",
-    "no-unused-expressions": "warn",
-    semi: "warn",
   },
   ignorePatterns: ["out", "dist", "**/*.d.ts"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
 };
