878415a4ee3b5f6d3112659d444e99f8fe395800
KOBAYASHI Ryota <koba.pic@gmail.com>
2021-11-29 18:26:54 +0900

    add typecheck command

--- src/const.ts
+++ src/const.ts
@@ -3,4 +3,5 @@ export const EXTENSION_NAME = "wraikny.wraikny-satysfi-extension";
 export const CONFIG_SCOPE = "satysfi";
 
 export const COMMAND_BUILD = "satysfi.build";
+export const COMMAND_TYPECHECK = "satysfi.typecheck";
 export const COMMAND_OPEN_BUILD_LOG = "satysfi.openBuildLog";
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import { commands, ExtensionContext } from "vscode";
 import { Builder } from "./builder";
-import { COMMAND_BUILD, COMMAND_OPEN_BUILD_LOG } from "./const";
+import { COMMAND_BUILD, COMMAND_OPEN_BUILD_LOG, COMMAND_TYPECHECK } from "./const";
 import { LanguageServer } from "./languageServer";
 import { Logger } from "./logger";
 import { packageCompletion } from "./packageCompletion";
@@ -30,5 +30,6 @@ export function activate(extContext: ExtensionContext): void {
   extContext.subscriptions.push(languageServer);
 
   commands.registerCommand(COMMAND_BUILD, () => builder.buildProject());
+  commands.registerCommand(COMMAND_TYPECHECK, () => typeChecker.checkCurrentDocument());
   commands.registerCommand(COMMAND_OPEN_BUILD_LOG, () => context.logger.showBuildLog());
 }
