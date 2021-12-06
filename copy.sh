f62283faf745bf4f7079817fec32c329dabca382
KOBAYASHI Ryota <koba.pic@gmail.com>
2021-12-06 18:00:29 +0900

    remove unnecessary files

--- copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-VSCODE_SATYSFI=~/.vscode/extensions/wraikny-vscode-satysfi
-cp -rf snippets $VSCODE_SATYSFI/.
-cp -rf syntaxes $VSCODE_SATYSFI/.
-cp -rf snippets $VSCODE_SATYSFI/.
-cp CHANGELOG.md $VSCODE_SATYSFI/.
-cp language-configuration.json $VSCODE_SATYSFI/.
-cp package.json $VSCODE_SATYSFI/.
-cp README.md $VSCODE_SATYSFI/.
