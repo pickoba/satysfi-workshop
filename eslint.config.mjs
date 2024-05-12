c184eadcbf101c3c895af028e938a16da5b97d89
KOBAYASHI Ryota <koba.pic@gmail.com>
2024-05-12 17:41:50 +0900

    migrate to eslint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { languageOptions: { globals: globals.node } },
+  { ignores: [".vscode-test/", "dist/", "out/", "tools/", "**/*.d.ts"] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+);
